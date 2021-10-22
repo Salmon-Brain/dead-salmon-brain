2282015ff89ebad2ac47d7e8078262eedb0c347b
livesalmonbrain <livesalmonbrain@gmail.com>
2021-10-22 22:29:07 +0300

    faster spark tests: share single spark context

--- src/test/scala/helpers/SparkHelper.scala
+++ src/test/scala/helpers/SparkHelper.scala
@@ -8,18 +8,21 @@ trait SparkHelper {
   Logger.getLogger("org").setLevel(Level.ERROR)
   Logger.getLogger("akka").setLevel(Level.ERROR)
 
-  lazy val conf = SparkHelper._sparkConf
-  lazy val spark = SparkHelper._spark
-  lazy val sc = SparkHelper._sc
-  lazy val sqlc = SparkHelper._sqlc
-}
-
-object SparkHelper {
   private lazy val _sparkConf = new SparkConf()
     .setMaster("local[1]")
     .setAppName(getClass.getName)
 
-  private lazy val _spark = SparkSession.builder().config(_sparkConf).getOrCreate()
-  private lazy val _sc = _spark.sparkContext
-  private lazy val _sqlc = _spark.sqlContext
+  lazy val spark: SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("spark session")
+      .config("spark.sql.shuffle.partitions", "1")
+      .config(_sparkConf)
+      .getOrCreate()
+  }
+
+  lazy val conf = spark.conf
+  lazy val sc = spark.sparkContext
+  lazy val sqlc = spark.sqlContext
 }
