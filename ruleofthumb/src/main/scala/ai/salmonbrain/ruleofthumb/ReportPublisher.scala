dd712439de93826db09fe212acc99611c7d99445
livesalmonbrain <livesalmonbrain@gmail.com>
2022-02-08 20:14:21 +0300

    exp-admin: pure toJson without jackson

--- ruleofthumb/src/main/scala/ai/salmonbrain/ruleofthumb/ReportPublisher.scala
+++ ruleofthumb/src/main/scala/ai/salmonbrain/ruleofthumb/ReportPublisher.scala
@@ -1,39 +1,30 @@
 package ai.salmonbrain.ruleofthumb
 
-import ai.salmonbrain.experiment.api.dto.ReportDto
 import org.apache.commons.io.IOUtils
 import org.apache.http.HttpHeaders
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.current_timestamp
-import org.apache.spark.sql.{DataFrame, Encoders}
-import org.codehaus.jackson.map.ObjectMapper
 import org.slf4j.LoggerFactory
 
 import java.nio.charset.StandardCharsets
 
 object ReportPublisher {
   private val log = LoggerFactory.getLogger("ReportPublisher")
-  private val objectMapper = new ObjectMapper();
-
-  private def toJson(dto: ReportDto): String = {
-    objectMapper.writeValueAsString(dto)
-  }
 
   private def sendReport(url: String, report: DataFrame): Unit = {
-    val reportEncoder = Encoders.bean(classOf[ReportDto])
     val encodedReport = report
       // TODO: replace current_timestamp with real report period end?
       .withColumn("ts", current_timestamp())
-      .as(reportEncoder)
 
-    encodedReport.foreachPartition((pIt: Iterator[ReportDto]) => {
+    encodedReport.toJSON.foreachPartition((pIt: Iterator[String]) => {
       pIt.foreach(dto => {
         val client = HttpClientBuilder.create().build()
         val post = new HttpPost(url)
         post.addHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-        post.setEntity(new StringEntity(toJson(dto)))
+        post.setEntity(new StringEntity(dto))
 
         val response = client.execute(post)
         val entity = response.getEntity
