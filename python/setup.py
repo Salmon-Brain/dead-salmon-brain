69dfabdffbab8d3bb7ae982d097268627b3b8b29
deadsalmonbrain <deadsalmonbrain@gmail.com>
2022-03-21 18:08:46 +0300

    up version

--- python/setup.py
+++ python/setup.py
@@ -9,7 +9,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name="dead-salmon-brain",
-    version="0.0.5",
+    version="0.0.6",
     description="Dead Salmon Brain is a cluster computing system for analysing A/B experiments",
     license="Apache License v2.0",
     author="Dead Salmon Brain",
