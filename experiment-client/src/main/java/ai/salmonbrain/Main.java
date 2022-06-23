53839d9888d4b0a12f0dfff9767261c064d7c6cd
livesalmonbrain <livesalmonbrain@gmail.com>
2022-06-23 20:23:27 +0300

    clean push

--- experiment-client/src/main/java/ai/salmonbrain/Main.java
+++
@@ -1,7 +0,0 @@
-package ai.salmonbrain;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- experiment-client/src/test/java/ai/salmonbrain/experiment/ExperimentConfigSelectorTest.java
+++ experiment-client/src/test/java/ai/salmonbrain/experiment/ExperimentConfigSelectorTest.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class ExperimentConfigSelectorTest {
